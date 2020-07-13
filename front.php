9bf9220cfdb44fc99d83706401575b9acbf94682
Laurine-lefort <laurine.le-fort@iseg.fr>
2020-07-13 10:49:26 +0200

    dev blog

--- front.php
+++ front.php
@@ -9,6 +9,22 @@
     <h1> Travel Blog</h1>
 </header>
 <body>
+<a href="http://localhost:8888/ISCC-2020/ISCC-2020-MyDevBlog/front.php?page=1">Accueil !</a>
+  <a href="http://localhost:8888/ISCC-2020/ISCC-2020-MyDevBlog/front.php?page=2">Articles</a>
+  <a href="http://localhost:8888/ISCC-2020/ISCC-2020-MyDevBlog/front.php?page=3">Contact</a>
+    <?php
+    session_start();
+    if($_GET['page'] == 1){
+       echo'<h1>Accueil !</h1>';
+       
+    }
+    elseif($_GET['page'] ==2){
+        echo'<h1>Articles</h1>';
+    }
+    elseif($_GET['page'] ==3){
+        echo'<h1>Contact</h1>';
+    }
+    ?>
 
 </body>
 <footer>
