5e32421b1dfaf593ab2c033030945b510226b110
Laurine-lefort <laurine.le-fort@iseg.fr>
2020-07-13 12:44:02 +0200

    dev blog

---
+++ page-accueil.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<header>
+    <h1> Mon projet </h1>
+</header>
+<body>
+</body>
+<footer>
+</footer>
+</html>
