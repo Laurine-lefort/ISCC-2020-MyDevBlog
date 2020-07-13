ea3649ec87f0d301dde376813206663de04520f3
Laurine-lefort <laurine.le-fort@iseg.fr>
2020-07-13 10:25:35 +0200

    dev blog

---
+++ front.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<header>
+    <h1> Travel Blog</h1>
+</header>
+<body>
+
+</body>
+<footer>
+
+</footer>
+</html>
