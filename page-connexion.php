c9f2f41616800db96bf4b4a5b8ce9beb4ff0fa62
Laurine-lefort <laurine.le-fort@iseg.fr>
2020-07-16 12:32:15 +0200

    DEV BLOG

--- page-connexion.php
+++ page-connexion.php
@@ -7,22 +7,19 @@
 </head>
 <?php include("header1.php")?>
 <header>
-    <h2> Connexion </h2>
-</header>
-    <body>
-        <div id="after_submit"></div>
-<form id="page-connexion" action="page-connexion.php" method="POST" enctype="multipart/form-data">
-  <div class="row">
-    <label class="required" for="login">Login:</label><br />
-    <input id="login" class="input" name="login" type="text" value="" size="30" /><br />
-    <span id="login_validation" class="error_message"></span>
-  </div>
-  <div class="row">
-    <label class="required" for="password">Password:</label><br />
-    <input id="password" class="input" name="password" type="text" value="" size="30" /><br />
-    <span id="password_validation" class="error_message"></span>
-  </div>
-    <input id="submit_button" type="submit" value="Identification" />
+    <h2>Connexion</h2>
+<form method="POST" action="securite.php">
+    <label for="login">Login</label>
+<div id="login">
+    <input type="text" name="login" id="login">
+</div>
+    
+    <label for="password">Password</label>
+<div id="password">
+    <input type="text" name="password" id="password">
+</div>
+
+<input type="submit" value="Envoyer">
 </form>
     </body>
 </html>
