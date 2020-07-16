5b3d6d563e4aee84f6142cba34e078d8e0336b6e
Laurine-lefort <laurine.le-fort@iseg.fr>
2020-07-16 15:29:56 +0200

    dev blog

--- page-ajout-articles.php
+++ page-ajout-articles.php
@@ -2,11 +2,60 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Titre de la page</title>
+  <title>Ajouter un article</title>
   <link rel="stylesheet" href="style.css">
 </head>
-<?php include("header1.php")?>
-<header>
-    <h2> Mon projet </h2>
-</header>
+<?php include("header.php")?>
+<body>
+    <form method="POST">
+        <fieldset>
+            <p> Ecrire un article :</p>
+            <label for="Titre">Titre</label>
+            <div id="Titre">
+                <input type="text" name="Titre" id="Titre">
+            </div>
+            <div id="Image">
+                <input type="blomb" name="Image" id="Image">
+            </div>
+            <div id="Auteur">
+                <input type="text" name="Auteur" id="Auteur">
+            </div>
+
+        <textarea name="ajouter_article" placeholder="Votre message"></textarea><br/>
+
+        <br><input type="submit" value="Envoyer">
+</fieldset>
+</form>
+</br>
+<?php if(isset($message)) { echo $message; } ?>
+</body>
+<?php
+function connect_to_database() {    
+    $servername = "localhost"; 
+    $username = 'root';    
+     $password = 'root'; 
+     $databasename = "articles"; 
+ try {      
+       $pdo = new PDO("mysql:host=$servername;dbname=$databasename", $username, $password);  
+       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        echo"Connection réussie";
+        return $pdo;
+       } 
+       catch (PDOException $e) 
+       {        
+            echo "Connection failed : ". $e->getMessage();  
+              } 
+           }
+        if(isset($_POST ['Titre'], $_POST['ajouter_article'])) {
+            if(!empty($_POST['Titre']) AND !empty($_POST['ajouter_article'])) {
+                $titre= htmlspecialchars($_POST['Titre']);
+                $red_article= htmlspecialchars($_POST['ajouter_article']);
+                $ins= $bdd->prepare('INSERT INTO articles (Titre,Image,Date de publication,Auteur, Contenu texte,Extrait) VALUES(?,?,NOW(),?,?,?)');
+                $ins->execute(array($titre, $red_article));
+                $message= 'Votre article est posté !';
+            } else {
+                $message= 'Veuillez remplir tous les champs';
+            }
+        }
+           ?>
 </html>
--- securite.php
+++ securite.php
@@ -11,19 +11,49 @@
                    echo "<h3>Connected successfully<h3/>";   
           
              }  
-                    catch (PDOException $e) {        
-                        echo "<h3>Connection failed</h3>". $e->getMessage();  
-                          } 
+            catch (PDOException $e) {        
+                    echo "<h3>Connection failed</h3>". $e->getMessage();  
+            } 
 
-  $sth->excecute(array('admin','login'));
-  $login =$sth->fetchAll();
-
-  $sth->execute(array('2020', 'password'));
-  $password= $sth->fetchAll();
+         }
+            function login ($pdo)
+             {
+             try
+            {
+             if (!empty($_POST['loginn'])&& !empty($_POST['passwordd']))
+            {
+            $login=$_POST['loginn'];
+             $password=$_POST['passwordd'];
+                        
+           $requete=$pdo->query("SELECT passwordd
+            FROM utilisateurs
+             WHERE loginn='$login'");
+            $res=$requete->fetchAll();
+                        
+            if ($res)
+             {
+            if($password == $res[0]['passwordd'])
+             {
+              echo "Bonjour ".$_POST['loginn']." !";
+               echo '<br>';
+              echo "Vous êtes connectés";
+              echo '<br>';
+        header("page-ajout-articles.php");
+              }
+             }
+     else
+             {
+     echo '<p>mauvais couple identifiant / mot de passe.</p>';
      }
-
-$pdo=connected_data();
-
-
-  
-    ?>
+       }
+        }
+    catch(PDOException $e)
+{
+    echo "Login erreur" .$e->getMessage();
+ }
+}
+                        
+ $pdo= connect_to_database();
+ login($pdo);
+?>
+</html>
